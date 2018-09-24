a4407e4521da48ec4c4fcb2c260edc2031a17af7
Kieu Cao Chien <chienkcth1809013@fpt.edu.vn>
2018-09-24 18:34:30 +0700

    Khoi tao project

---
+++ cmake-build-debug/CMakeFiles/t1809e.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/t1809e.dir/main.c.obj"
+  "t1809e.pdb"
+  "t1809e.exe"
+  "t1809e.exe.manifest"
+  "libt1809e.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/t1809e.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
